1c936b6cc24ad66ad698d799bd44befab3fb1ea0
Bibhas1999 <bibhasash@gmail.com>
2023-03-08 12:52:55 +0530

    changes

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.listen(port,()=>{
 let options = {
     timezone: "Asia/Kolkata"
 }
-Job.schedule("35 1 * * 0-5", async ()=>{
+Job.schedule("10 13 * * 0-5", async ()=>{
     console.log("hi")
     let ac = await automateClockIn()
     console.log(ac)
